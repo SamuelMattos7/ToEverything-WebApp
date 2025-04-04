d7f296a79e08670123be5ff1d4d787cf66b45032
SamuelMattos7 <samuel.mattos@inacapmail.cl>
2025-04-04 16:26:19 -0300

    Projects API
    Created projects views and serializers for delete, read, and creation, as well as updating projects model

--- To-Do-App/projects/serializers.py
+++ To-Do-App/projects/serializers.py
@@ -5,3 +5,18 @@ class ProjectListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Projects
         fields = ['projectId', 'project_name']
+
+class ProjectCreationSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Projects
+        fields = ['project_name', 'creator', 'project_description', 'project_startDate', 'project_endDate', 'users']
+
+class ProjectDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Projects
+        fields = '__all__'
+
+class ProjectDeleteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Projects
+        fields = ['projectId']
--- To-Do-App/projects/views.py
+++ To-Do-App/projects/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+
+from task import serializer
 from .models import Projects
-from .serializers import ProjectListSerializer
+from .serializers import ProjectCreationSerializer, ProjectDeleteSerializer, ProjectDetailSerializer, ProjectListSerializer
 from rest_framework.response import Response
 # Create your views here.
 
@@ -12,3 +14,34 @@ def projectsListView(request):
     list_projects = Projects.objects.filter(creator=request.user)
     serializer = ProjectListSerializer(list_projects, many=True)
     return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def projectCreationView(request):
+    data = request.data.copy()
+    data['user'] = request.user.UserID
+    serializer = ProjectCreationSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save(creator=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def projectDetailsView(request, id):
+    try:
+        project = Projects.objects.get(projectId=id)
+        serializer = ProjectDetailSerializer(project)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except Projects.DoesNotExist:
+            return Response({"error":"Project was not found"}, status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['DELETE'])
+def projectDeleteView(request, id):
+    try:
+        project = Projects.objects.get(projectId=id)
+        serializer = ProjectDeleteSerializer(project)
+        project.delete()
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+    except Projects.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
